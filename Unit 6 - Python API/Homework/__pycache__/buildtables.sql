d5ec557b41f0ac41b1821657c0c1fb1abbf2f948
kadihunter <christy.mckenna@gmail.com>
2019-12-10 19:47:51 -0500

    Assignment Complete!

---
+++ Unit 6 - Python API/Homework/__pycache__/buildtables.sql
@@ -0,0 +1,16 @@
+CREATE TABLE Employees (
+	emp_no INT(6) NOT NULL
+	, birth_date DATE NOT NULL
+	, first_name VARCHAR(20) NOT NULL
+	, last_name VARCHAR(20) NOT NULL
+	, gender VARCHAR(1) NOT NULL
+	, hire_date DATE NOT NULL
+);
+	
+
+CREATE TABLE Departments (
+	dept_no VARCHAR(4) NOT NULL
+	, dept_name VARCHAR(30) NOT NULL
+);
+
+
